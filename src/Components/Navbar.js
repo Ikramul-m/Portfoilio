188528f937449cd01939113713693e82ea6f25a4
Ikramul-m <i20727172@gmail.com>
2023-07-22 15:03:21 +0530

    Third commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+
+    return (
+        <div style={{backgroundColor: 'bisque' , height: '5rem'}}>
+
+        </div>
+    )
+}
+
+export default Navbar
