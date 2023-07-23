b47d3d9ebd6d990c7e686f5293de64a59ed16be1
Ikramul-m <i20727172@gmail.com>
2023-07-23 12:03:53 +0530

    css

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -5,7 +5,15 @@ const Hero = () => {
 
     return (
         <div className='Hero'>
-            
+            <div className='home-content'>
+
+                <h1>Hi, I'm <span>Ikram</span></h1>
+                <h3>Frontend Developer</h3>
+                <p>I was enthusiastic in competitive coding and data structure and algorithms but ended up being a
+                    developer.Learnt and expertised frontend developement within 3 months by working at Hiclousia
+                    Staffing and Recruiting (IIT Madras Research Park).</p>
+
+            </div>
         </div>
     )
 }
